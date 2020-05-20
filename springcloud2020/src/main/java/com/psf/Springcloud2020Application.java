d056e134c1af3be94809f0f562aa127fe7a12f57
peng <2498597150@qq.com>
2020-05-20 11:17:58 +0800

    my first commit

---
+++ springcloud2020/src/main/java/com/psf/Springcloud2020Application.java
@@ -0,0 +1,13 @@
+package com.psf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud2020Application.class, args);
+    }
+
+}
---
+++ springcloud2020/src/test/java/com/psf/Springcloud2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
